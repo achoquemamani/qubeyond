70611ed6da84de95e8785bae1b8b6e5d1698b23c
Ariel Choque Mamani <ariel.choquemamani@balloon-group.com>
2023-10-23 01:44:31 -0300

    feat: add component for checking internet

---
+++ src/components/CheckInternet.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-btn
+    size="xs"
+    :disabled="!hasInternet"
+    round
+    color="primaryblue-5"
+    icon="fa fa-wifi"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'CheckInternet',
+  setup() {
+    const hasInternet = ref(navigator.onLine);
+    return {
+      hasInternet
+    };
+  }
+});
+</script>
